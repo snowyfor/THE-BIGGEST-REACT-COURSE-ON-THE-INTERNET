d9fc0f025f5bc3bc7a8b902f909a8feda85c614d
Snowy Forest <134951391+snowyfor@users.noreply.github.com>
2025-02-02 09:06:41 +0700

    Update ProductList.jsx

--- 01. Fundamentals/06. List of Data/exercise/solution/components/ProductList.jsx
+++ 01. Fundamentals/06. List of Data/exercise/solution/components/ProductList.jsx
@@ -6,14 +6,14 @@ const ProductList = () => {
   ];
 
   return (
-    <div>
+    <>
       {products.map((p) => (
         <div key={p.id}>
           <h1>Name: {p.name}</h1>
           <h1>Price: {p.price}</h1>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
